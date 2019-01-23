5d7a574816c3945d72a9a16a996a9e5c5e6c8a9d
zaki abbasi chotu lapi <zakiabbasi15@yahoo.com>
2019-01-23 16:14:48 +0500

    Admin login integrated with APi

--- README.md
+++ README.md
@@ -11,4 +11,6 @@ Login and Register can be opened from NAVBAR directly.
 
 user-signup integrated with API. Provide Credentials click register. if success redirect to Landing page. else stay on register pop-up page and displays response message in alert-box(not designed yet).
 
- User Login integrated with API. Provide Credentials click register. if success redirect to Landing page. else stay on lohin pop-up page and displays response message in alert-box(not designed yet).
+ User Login integrated with API. Provide Credentials click login. if success redirect to Landing page. else stay on lohin pop-up page and displays response message in alert-box(not designed yet).
+
+ admin Login integrated with API. Go to route admin-page Provide Credentials click login. if success redirect to admin dashboard. Displays response page in alert box.
--- src/views/AdminPage/AdminLogin.jsx
+++ src/views/AdminPage/AdminLogin.jsx
@@ -5,122 +5,165 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
-
+import axios from 'axios';
 // import { Redirect } from 'react-router';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
 // import AdminPage from './AdminPage.jsx'
 
-import { updateAdmin } from '../../actions/admin-action.js'
+import { updateAdmin } from '../../actions/admin-action.js';
 import { bindActionCreators } from 'redux';
 
 class AdminLogin extends React.Component {
-    
-    
-    constructor(props){
-        super(props);
-    
-        this.state={
-            // redirect:false,
-            email:'',
-            password:'',
-            error:false
-        }
-    }
+  constructor(props) {
+    super(props);
 
-            adminLoginResponse = () => { 
-          
-            };
-            handleEmailChange = event => {
-                this.setState({
-                email:event.target.value,
-                });
-            };
-            handlePasswordChange = event => {
-                this.setState({
-                password:event.target.value,
-                });
-            };
-            buttonClick = () => {
-                    if(this.state.email==='abc' && this.state.password==='123')
-                    {
-                        (this.props.onUpdateAdmin(true))
-                                this.setState({
-                                error:false,
-                            })
-                    }
-                    else{
-                        this.setState({
-                                error:true
-                                    })
-                    }
-            }
-
-            render() { 
-            const cardStyle = {
-                maxWidth: 345,
-            }
-            const itemsAlign = {
-                    position: 'absolute', left: '50%', top: '50%',
-                    transform: 'translate(-50%, -50%)'
-                            }
-            
-                // if(this.state.redirect){
-                //     return (<Redirect to='/admin-page' Component={AdminPage}/>)
-                // }
-                return (   
-                <Grid  style={itemsAlign} container alignItems='center' justify='center' direction='column-reverse'>
-                <Grid item xs={12} sm={12} md={12}>
-                <Card style={cardStyle}>
-                    <CardContent>
-                        <TextField
-                        id="outlined-email-input"
-                        label="Email"
-                        type="email"
-                        name="email"
-                        autoComplete="email"
-                        margin="normal"
-                        variant="outlined"
-                        onChange={e=>{this.handleEmailChange(e)}}
-                        fullWidth
-                        error={this.state.error}
-                        />
-                        <TextField
-                        id="outlined-password-input"
-                        label="Password"
-                        type="password"
-                        autoComplete="current-password"
-                        margin="normal"
-                        variant="outlined"
-                        onChange={e=>{this.handlePasswordChange(e)}}
-                        fullWidth
-                        error={this.state.error}
-                        />
-                    </CardContent>
-                    <CardActions>
-                    <Button outlined size="small" color="primary" onClick={()=>this.buttonClick()}>
-                        Sign In
-                    </Button>
-                    </CardActions>
-                </Card>
-                </Grid>
-                </Grid>
-        )
-    }
-}
-
-const mapStatetoProps = (state) => {
-    
-    return (
-        { loggedIn:state.admin }
-
-    )
-}
-
-const mapActionsToProps = (dispatch,props) => {
-    console.log(props)
-    return bindActionCreators({
-      onUpdateAdmin:updateAdmin
-    },dispatch);  
+    this.state = {
+      // redirect:false,
+      email: '',
+      password: '',
+      error: false
+    };
   }
 
-export default connect(mapStatetoProps,mapActionsToProps)(AdminLogin)
+  adminLoginResponse = () => {};
+  handleEmailChange = event => {
+    this.setState({
+      email: event.target.value
+    });
+  };
+  handlePasswordChange = event => {
+    this.setState({
+      password: event.target.value
+    });
+  };
+  buttonClick = () => {
+    const data = {
+      email: this.state.email,
+      password: this.state.password
+    };
+    axios
+      .post(
+        'http://localhost:8000/api/admin/adminlogin',
+        data
+        //, { headers: {"Authorization" : `Bearer ${token}`} }
+      )
+      .then(res => {
+        JSON.stringify(res);
+        console.log(res.data);
+        //Check if response reture suceess: true or false
+        if (res.data.success === false) {
+          alert(res.data.message);
+          this.setState({ error: true });
+        } else {
+          //redirect to login component
+          alert('Succesfully Logged in');
+          this.props.onUpdateAdmin(true);
+          this.setState({
+            error: false
+          });
+        }
+      })
+      .catch(error => {
+        alert('Internal Server error, Server Resopnded with "' + error + '"');
+      });
+
+    if (this.state.email === 'abc' && this.state.password === '123') {
+      this.props.onUpdateAdmin(true);
+      this.setState({
+        error: false
+      });
+    } else {
+      this.setState({
+        error: true
+      });
+    }
+  };
+
+  render() {
+    const cardStyle = {
+      maxWidth: 345
+    };
+    const itemsAlign = {
+      position: 'absolute',
+      left: '50%',
+      top: '50%',
+      transform: 'translate(-50%, -50%)'
+    };
+
+    // if(this.state.redirect){
+    //     return (<Redirect to='/admin-page' Component={AdminPage}/>)
+    // }
+    return (
+      <Grid
+        style={itemsAlign}
+        container
+        alignItems="center"
+        justify="center"
+        direction="column-reverse"
+      >
+        <Grid item xs={12} sm={12} md={12}>
+          <Card style={cardStyle}>
+            <CardContent>
+              <TextField
+                id="outlined-email-input"
+                label="Email"
+                type="email"
+                name="email"
+                autoComplete="email"
+                margin="normal"
+                variant="outlined"
+                onChange={e => {
+                  this.handleEmailChange(e);
+                }}
+                fullWidth
+                error={this.state.error}
+              />
+              <TextField
+                id="outlined-password-input"
+                label="Password"
+                type="password"
+                autoComplete="current-password"
+                margin="normal"
+                variant="outlined"
+                onChange={e => {
+                  this.handlePasswordChange(e);
+                }}
+                fullWidth
+                error={this.state.error}
+              />
+            </CardContent>
+            <CardActions>
+              <Button
+                outlined
+                size="small"
+                color="primary"
+                onClick={() => this.buttonClick()}
+              >
+                Sign In
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+      </Grid>
+    );
+  }
+}
+
+const mapStatetoProps = state => {
+  return { loggedIn: state.admin };
+};
+
+const mapActionsToProps = (dispatch, props) => {
+  console.log(props);
+  return bindActionCreators(
+    {
+      onUpdateAdmin: updateAdmin
+    },
+    dispatch
+  );
+};
+
+export default connect(
+  mapStatetoProps,
+  mapActionsToProps
+)(AdminLogin);
