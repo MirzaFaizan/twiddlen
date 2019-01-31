cfe9a377ab55d9df187459787bf5d264f36e5bde
Faizan <mirzafaizanejaz@gmail.com>
2019-01-31 12:45:04 +0500

    workbox working, precaching stratergies implemented

--- src/index.js
+++ src/index.js
@@ -1,51 +1,56 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { createBrowserHistory } from "history";
-import { Router, Route, Switch } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createBrowserHistory } from 'history';
+import { Router, Route, Switch } from 'react-router-dom';
 
-import indexRoutes from "routes/index.jsx";
+import indexRoutes from 'routes/index.jsx';
 
-import "assets/scss/material-kit-react.css?v=1.3.0";
-// import registerServiceWorker from './registerServiceWorker';
+import 'assets/scss/material-kit-react.css?v=1.3.0';
+import registerServiceWorker from './registerServiceWorker';
 
-
-import {combineReducers,createStore} from 'redux';
-import {Provider} from 'react-redux'
+import { combineReducers, createStore } from 'redux';
+import { Provider } from 'react-redux';
 import productsReducer from './reducers/productsReducer.js';
 import usersReducer from './reducers/usersReducer.js';
-import adminReducer from './reducers/adminReducer.js'
+import adminReducer from './reducers/adminReducer.js';
 
 const allReducers = combineReducers({
-  products:productsReducer,
-  user:usersReducer,
-  admin:adminReducer
-})
+  products: productsReducer,
+  user: usersReducer,
+  admin: adminReducer
+});
 
 const store = createStore(
-  allReducers , 
+  allReducers,
   {
-  products:[{name:'iPhone'}],
-  user:'Michael',
-  admin:false
-  },
+    products: [{ name: 'iPhone' }],
+    user: 'Michael',
+    admin: false
+  }
   // window.devToolsExtension && window.devToolsExtension()
-
 );
 // console.log(store.getState());
 
-
 var hist = createBrowserHistory();
 
-
 ReactDOM.render(
-  <Provider store={store} >
-  <Router history={hist} > 
-    <Switch >
-      {indexRoutes.map((prop, key) => {
-        return <Route path={prop.path} key={key} component={prop.component} randomProps="randomProps"/>;
-      })}
-    </Switch>
-  </Router>
+  <Provider store={store}>
+    <Router history={hist}>
+      <Switch>
+        {indexRoutes.map((prop, key) => {
+          return (
+            <Route
+              path={prop.path}
+              key={key}
+              component={prop.component}
+              randomProps="randomProps"
+            />
+          );
+        })}
+      </Switch>
+    </Router>
   </Provider>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
+
+registerServiceWorker.register();
